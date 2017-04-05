9f7b361b5d835328a70147d94e5f2288157ecab9
eak1537 <eak1537@hotmail.com>
2017-04-05 14:56:46 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Tubtim Farm</title>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'/>
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+.wrapper .page .left-column .dark-panel .dark-panel-center ul {
+}
+</style>
+</head>
+<body>
+<div class="wrapper">
+  <div class="logo-menu-container">
+    <div class="logo">ทับทิมสุกรฟาร์ม</div>
+    <div class="menu">
+      <ul>
+        <li><a href="index.php">หน้าแรก</a></li>
+        <li>เมนูหลัก</li>
+        <li><a href="insert.php">สมัครสมาชิก</a></li>
+        <li>เกี่ยวกับ</li>
+        <li class="nobg"><a href="login.php">เข้าสู่ระบบ</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="searchform-container">
+    <div class="searchform-content">ฟาร์มสุกร พ่อพันธุ์ แม่พันธุ์ ลูกสุกรขุน สุกรขุน</div>
+  </div>
+  <div class="clear"></div>
+  <div class="page">
+    <div class="main-banner"><img src="images/banner.jpg" alt="banner" /></div>
+    <div class="clear"></div>
+    <div class="left-column">
+      <div class="dark-panel">
+        <div class="dark-panel-top"></div>
+        <div class="dark-panel-center">
+          <ul>
+            <ul>
+              
+              <ul>
+                  <li>
+                    <h1> ประกาศ !!!</h1>
+                  </li>
+              </ul>
+            </ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+<p>&nbsp;</p>
+          <p>หากยังไม่เป็นสมาชิก ??</p>
+          <p>อาจไม่สามารถดูข้อมูลในฟาร์มข้าง</p>
+          <p>ในได้ กรุณาสมัครสมาชิกก่อน</p>
+          <p>&nbsp; </p>
+          <ul>
+            <li>
+              <div class="button"><a href="insert.php">สมัครสมาชิก</a></div>
+            </li>
+          </ul>
+        </div>
+        <p>&nbsp;</p>
+        <div class="dark-panel-bottom"></div>
+      </div>
+      <div class="light-panel">
+        <div class="light-panel-top"></div>
+        <div class="light-panel-center">
+          <h1>เมนูหลัก</h1>
+          <ul>
+            <li>+ ข่าวสาร</li>
+            <li>+ ข้อมูลในฟาร์ม</li>
+            <li>+ แนะนำการเลี้ยงสุกรเบื้องต้น</li>
+            <li>+ ราคาสุกร ราคาวัตถุดิบ</li>
+          </ul>
+        </div>
+        <div class="light-panel-bottom"></div>
+      </div>
+      <div class="dark-panel">
+        <div class="dark-panel-top"></div>
+        <div class="dark-panel-center">
+          <ul>
+            <li></li>
+            <li class="date"> เจ้าของฟาร์ม</li>
+            <li class="news"></li>
+            <li class="news">นาย อติชาต มีภูมิ   </li>
+            <li class="news">โทรศัพท์ 088-783-0530</li>
+          </ul>
+        </div>
+        <div class="dark-panel-bottom"></div>
+      </div>
+    </div>
+    <div class="right-column">
+      <div class="right-column-content">
+        <div class="right-column-content-heading">
+          <h1>ฟาร์มสุกร หมายถึง ฟาร์มที่ผลิตสุกรขุนเพื่อการค้า ฟาร์มพ่อ-แม่พันธุ์เพื่อผลิตลูกสุกร และฟาร์มเลี้ยงสุกร</h1>
+          <h2>&nbsp;</h2>
+          <h2>การเลี้ยงสุกรในปัจุบัน</h2>
+          <p><strong>อาชีพการเลี้ยงสัตว์ที่นิยมเลี้ยงในหมู่บ้านต่างๆ</strong> ก็คือ<strong> การเลี้ยงสุกร หรือ</strong> <strong>การเลี้ยงหมู</strong><br />
+ทั้งนี้เพราะสามารถเลี้ยงเป็นฟาร์มเล็กๆจำนวนไม่กี่ตัวก็ได้ เพราะใช้พื้นที่น้อยเลี้ยงง่ายใช้แรงงานน้อยและสามารถนำเศษอาหารมาใช้เป็นอาหารของสุกรได้นอกจากนี้มูลสุกรยังสามารถนำมาเป็นปุ๋ยหรือ อาหารในบ่อเลี้ยงปลาได้ สุกรเป็นสัตว์เลี้ยงที่ขยายพันธุ์ได้เร็วมีลูกดกจึงเป็นกิจการที่ให้ผลกำไรดี ทำให้เป็นที่นิยมเลี้ยงกันมาก</p>
+        </div>
+      </div>
+      <div class="right-column-content">
+        <div class="right-column-content-heading">
+          <h1>&nbsp;</h1>
+          <h2>ลูกหมูขุนที่เลี้ยงในฟาร์มมีมาตรฐาน สมบูรณ์ แข็งแรง ปลอดโรค ได้น้ำหนักดี</h2>
+        </div>
+        <div class="right-column-content-img-left"> <img src="images/img-1.png" alt="banner" /></div>
+        <div class="right-column-content-content">
+          <p>การเลี้ยง และการดูแลลูกหมู ตั้งแต่หลังคลอด ไปจนถึงหย่านม นับได้ว่า เป็นช่วงที่มีความยุ่งยากมากกว่าช่วงอื่นๆ เป็นช่วงที่มีการสูญเสียลูกหมูมากที่สุด โดยเฉลี่ยประมาณ ๒ ตัวต่อครอก ซึ่งอาจเกิดเนื่องจากถูกแม่ทับตาย ท้องร่วงอย่างแรง อดอาหาร โลหิตจาง และเสียเลือดมากทางสายสะดือเมื่อคลอด อย่างไรก็ตามการสูญเสียลูกหมูในช่วงนี้ ส่วนใหญ่เนื่องมาจากการดูแลไม่ดี มากกว่าสาเหตุอื่นๆ ดังนั้น เพื่อลดการสูญเสีย ลูกหมูในช่วงนี้ ผู้เลี้ยงควรเตรียมการดูแลลูกหมู</p>
+        </div>
+      </div>
+      <div class="right-column-content">
+        <div class="right-column-content-heading">
+          <h2>พ่อพันธุ์-แม่พันธุ์หมู ภายในฟาร์มมีคุณภาพ สมบูรณ์ แข็งแรง</h2>
+        </div>
+        <div class="right-column-content-content">
+          <p>หมูสาวเจริญเติบโตในลักษณะเช่นเดียวกันกับหมูหนุ่ม เมื่อมีอายุมากขึ้น ปริมาณของไข่ที่ตกจากรังไข่จะเพิ่มขึ้น ทำให้ปริมาณลูกที่คลอดมีจำนวนมาก จนกระทั่งแม่หมูมีอายุประมาณ ๑๕ เดือน ปริมาณไข่ที่ตกก็จะคงที่ โดยปกติจะผสมพันธุ์หมูสาว เมื่อมีอายุได้ราว ๗-๘ เดือน หรือน้ำหนักราว ๑๑๐-๑๑๕ กิโลกรัม (สำหรับหมูพันธุ์พื้นเมืองประมาณ ๘๐-๙๐ กิโลกรัม ขึ้นอยู่กับชนิดของหมูพันธุ์พื้นเมืองด้วย) การผสมพันธุ์แม่หมูที่มีขนาดเล็ก หรือมีอายุน้อยอยู่ จะทำให้อายุการใช้งานของแม่หมูตัวนั้นสั้นลง และจำนวนลูกที่ได้ก็น้อยด้วย ทั้งนี้ เนื่องจากการตกไข่จากรังไข่มีน้อย</p>
+        </div>
+        <div class="right-column-content-img-right"> <img src="images/img-2.png" alt="banner" /> </div>
+        <div class="clear right-cloumn-content-border"></div>
+        <div class="right-column-content-content">
+          <p>หมูตัวผู้ที่จะนำมาใช้ผสมพันธุ์ได้ดี ควรมีสุขภาพแข็งแรงสมบูรณ์ มีขนาดและอายุที่พอเหมาะ เพื่อให้สามารถผลิตน้ำเชื้อที่มีคุณภาพดีพอเพียง มีผลทำให้การผสมติดสูง และได้จำนวนลูกมากเป็นปกติ แม้ว่าหมูตัวผู้สามารถผลิตอสุจิได้บ้างแล้ว เมื่อมีอายุย่างเข้า ๔ เดือนก็ตาม แต่จำนวนน้ำเชื้อ และอสุจิที่ผลิตได้ จะเพิ่มมากขึ้น เมื่อหมูมีอายุมากขึ้น ดังนั้น หมูตัวผู้ที่จะนำมาใช้ผสมพันธุ์ ควรมีขนาดใหญ่พอเหมาะที่จะผสมกับแม่หมูที่มีขนาดปกติได้ โดยปกติมักใช้ผสมพันธุ์ ได้เมื่อมีอายุราว ๘ เดือน และมีน้ำหนักประมาณ ๘๐-๙๐ กิโลกรัม สำหรับหมูพ่อพันธุ์ต่างประเทศ ควรมีน้ำหนักราว ๑๑๕ กิโลกรัม</p>
+        </div>
+        <div class="right-column-content-img-right right-column-content-img-right-margin-bottom-none"> <img src="images/img-3.png" alt="banner" /> </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="footer-wrapper">
+  <div class="footer-top"></div>
+  <div class="footer-center">
+    <div class="footer-content-left">
+      <h1>ทับทิมสุกรฟาร์ม</h1>
+      <h2>ฟาร์มสุกร พ่อพันธุ์ แม่พันธุ์ ลูกสุกร สุกรขุน</h2>
+      <p>มาตรฐานฟาร์มเลี้ยงสุกรนี้ กำหนดขึ้นเป็นมาตรฐานเพื่อให้ฟาร์มที่ต้องการขึ้นทะเบียนเป็นฟาร์มที่ได้มาตรฐานเป็นที่ยอมรับ ได้ ยึดถือปฏิบัติ เพื่อให้ได้การรับรองจากกรมปศุสัตว์ ซึ่งมาตรฐานนี้เป็นเกณฑ์มาตรฐานขั้นพื้นฐานสำหรับฟาร์มที่จะได้การรับรอง</p>
+      <p>มาตรฐานฟาร์มเลี้ยงสุกรนี้กำหนดวิธีปฏิบัติด้านการจัดการฟาร์ม การจัดการด้านสุขภาพสัตว์ และการจัดการด้านสิ่งแวดล้อม เพื่อให้ได้สุกรที่ถูกสุขลักษณะ และเหมาะสมต่อผู้บริโภค</p>
+    </div>
+    <div class="footer-content-right">
+      <h1>&nbsp;</h1>
+      <h1>ที่อยู่ฟาร์ม </h1>
+      <p>&nbsp;</p>
+<p>ตั้งอยู่ ตำบล นาขยาด อำเภอ ควนขนุน จังหวัด พัทุลง 93110    </p>
+<p>อีเมลล์: tumtim_farm@gmail.com</p>
+<p>โทรศัพท์: 074-631-349</p>
+    </div>
+  </div>
+  <div class="footer-bottom"></div>
+</div>
+<div class="clear"></div>
+<div class="copyrights">Copyright (c) Web Programming. Design by Pitpipat Ramthong. Photos by JameBall
+  <div class="copyrights-bottom"></div>
+</div>
+</body>
+</html>
